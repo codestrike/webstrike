2c1f9b81cf5e8473f3c365b96f95b2ed5b7db01f
Amit Menon <dcorelibran@gmail.com>
2014-09-14 15:34:00 +0530

    TRIED A LAYOUT ADDED TEAM INFO.ERROR.MEMBERS ARE REPEATED TWICE

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ webstrike
 =========
 
 Heroku connected web app of codestrike
+i want a single row layout for immortals 
+and 2 in a row layout for other members bt mistake hai.
+ also how to increment the index please correct
+ 
--- static/script/codestrike.js
+++ static/script/codestrike.js
@@ -59,7 +59,7 @@ codestrike.controller('homeCtrl', ['$scope', '$filter', function($scope, $filter
 }]);
 
 codestrike.controller('aboutCtrl', ['$scope', function($scope) {
-  $scope.members = [
+  $scope.immortals = [
   {
     'name':'Ashish Gaikwad',
     'post':'Founder and President',
@@ -70,6 +70,47 @@ codestrike.controller('aboutCtrl', ['$scope', function($scope) {
     'post':'Founder and Vice-President',
     'class':'BE CMPN 1',
     'info':'Parikshit (Pari) is competitive programming lover. Along with programming he also plays guitar everyday. According to him life is "Eat. Sleep. Code. Repeat".'
+    
+  },{
+    'name':'Amit Menon',
+    'post':'Awareness Head',
+    'class':'T.E CMPN 1',
+    'info':'Amit is an avid reader basically devours books. Extremely curious about working of things, and computers fascinated him since he was a kid. Also loves football,arts and music'
+  },{
+    'name':'Akshen Doke',
+    'post':'Technical Head',
+    'class':'T.E CMPN 2',
+    'info':'A versatile person who wants to do many things in lfe. Likes to question how and why.Wants to leave a mark on the world with his thoughts and ideas.'
+  },{
+    'name':'Vishmita Shetty',
+    'post':'C.E.O',
+    'class':'TE CMPN 2'
+  }];
+}]);
+
+codestrike.controller('aboutCtrl', ['$scope', function($sec) {
+  $sec.members = [
+  {
+    'name':'Ankit Champaneri',
+    'info':'Ankit is complety new to programming. Likes to play cricket and is a die hard follower. Keen to learn coding.'
+  },{
+    'name':'Pratik Gupta',
+    'info':'Pratik has a hobby to play videogames like Assassins Creed series hence found his way to Programming. Before code strike he has learned VB.Net, ASP.Net, JavaScript and also some introduction to C++'
+  },{
+    'name':'Shaishav Oza',
+    'info':'I am not exactly new to this programming genre, basic knowledge is known to me... Passionate, skillful, manager, fun loving, fun making are few out of the many qualities that define me. Android geek, Android enthusiast, not a follower, Atheist for life...'
+  },{
+    'name':'Atharva Joglekar',
+    'info':'New to programming.....loves to play cricket,football!Helpful,skillful manager,loves playing fifa, a good public speaker and man manager!'
+  },{
+    'name':'Tejas Nanaware',
+    'info':'I have good programming skills and I learnt basics in my 9th grade i.e. when I came to know basics of Java. Apart from that, I also know C++, Visual Basic, Instruction Set of 8085 and also DOS'
+  },{
+    'name':'Ravi Chandak',
+    'info':'not completely new to programming knows c++ and basics of HTML always curious to know how the things work and this curiosity is the reason for my die hard interest in programming '
+  },{
+    'name':'Ashraf Khan',
+    'info':'shraf is New to programming, a cricket , football player, starting to build up basics of C language. Hard working, passionate about gadgets and OS.curious to learn new and challenging things. Main focus is to build up a strong base in C++ in the next 6 months.'
   }];
 }]);
 
